6ce9bc33b391e46d4fc910b5cb8f3a3017e457cc
Jacky <jack@mrqu.net>
2020-04-23 12:43:59 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ wget https://github.com/traccar/traccar/releases/download/v4.8/traccar-other-4.8
 unzip -d /app traccar-other-4.8.zip
 rm traccar-other-4.8.zip
 
-CON="<?xml version='1.0' encoding='utf-8'?><!DOCTYPE properties SYSTEM 'http://java.sun.com/dtd/properties.dtd'><properties><entry key='config.default'>./conf/default.xml</entry><entry key='web.port'>$PORT</entry><entry key='database.driver'>org.postgresql.Driver</entry><entry key='database.url'>$JDBC_DATABASE_URL</entry><entry key='database.user'>$JDBC_DATABASE_USERNAME</entry><entry key='database.password'>$JDBC_DATABASE_PASSWORD</entry></properties>"
+CON="<?xml version='1.0' encoding='utf-8'?><!DOCTYPE properties SYSTEM 'http://java.sun.com/dtd/properties.dtd'><properties><entry key='config.default'>./conf/default.xml</entry><entry key='web.port'>$PORT</entry><entry key='database.driver'>org.postgresql.Driver</entry><entry key='database.url'>$JDBC_DATABASE_URL</entry><entry key='database.user'>$JDBC_DATABASE_USERNAME</entry><entry key='database.password'>$JDBC_DATABASE_PASSWORD</entry><entry key='database.maxPoolSize'>5</entry></properties>"
 CON_ESCAPED=${CON//&/&amp;}
 echo $CON_ESCAPED > ./conf/traccar.xml
 
